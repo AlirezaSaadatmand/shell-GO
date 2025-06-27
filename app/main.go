c0631cce71dea8a7a42136fa63c9d9538702fdea
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-06-27 17:28:23 +0330

    refactor and type command

--- app/main.go
+++ app/main.go
@@ -4,30 +4,71 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
+var COMMANDS map[string]func([]string)
 
+func init() {
+	COMMANDS = map[string]func([]string){
+		"exit": exit,
+		"echo": echo,
+		"type": type_,
+	}
+}
 func main() {
 	for {
-		// Uncomment this block to pass the first stage
 		fmt.Fprint(os.Stdout, "$ ")
-	
-		// Wait for user input
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-	
-		if command[:len(command) - 1] == "exit 0" {
-			os.Exit(0)
-		} else if strings.Split(command[:len(command) - 1], " ")[0] == "echo" {
-			fmt.Println(strings.Join(strings.Split(command[:len(command) - 1], " ")[1:], " "))
+		input = input[:len(input) - 1]
+		command := strings.Split(input, " ")[0]
+		args := strings.Split(input, " ")[1:]
+
+		if _, ok := COMMANDS[command]; ok {
+			COMMANDS[command](args)
 		} else {
-			fmt.Println(command[:len(command) - 1] + ": command not found")
+			fmt.Println(command + ": command not found")
 		}
+	}
+}
+
+func exit(args []string) {
+	if len(args) > 1 {
+		fmt.Println("Error: too many arguments")
+		return
+	}
+
+	status, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Invalid number:", err)
+		return
+	}
+	os.Exit(status)
+}
+
+func echo(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Error: not enough arguments")
+		return 
+	}
+	fmt.Println(strings.Join(args, " "))
+}
+
+func type_(args []string) {
+	if len(args) != 1 {
+		fmt.Println("Error: expected exactly one argument")
+		return
+	}
+
+	if _, exists := COMMANDS[args[0]]; exists {
+		fmt.Println(args[0] + " is a shell builtin")
+	} else {
+		fmt.Println(args[0] + ": not found")
 	}
 }
