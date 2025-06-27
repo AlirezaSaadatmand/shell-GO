5d0905e7f16e7a752f7f18cf59d3276c82ef854a
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-06-27 18:09:28 +0330

    updated type command

--- app/main.go
+++ app/main.go
@@ -4,17 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
 
 var COMMANDS map[string]func([]string)
+var builtin []string
 
 func init() {
 	COMMANDS = map[string]func([]string){
 		"exit": exit,
 		"echo": echo,
 		"type": type_,
+	}
+	builtin = []string{
+		"exit",
+		"echo",
+		"type",
 	}
 }
 func main() {
@@ -26,7 +33,7 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		input = input[:len(input) - 1]
+		input = input[:len(input)-1]
 		command := strings.Split(input, " ")[0]
 		args := strings.Split(input, " ")[1:]
 
@@ -39,8 +46,8 @@ func main() {
 }
 
 func exit(args []string) {
-	if len(args) > 1 {
-		fmt.Println("Error: too many arguments")
+	if len(args) != 1 {
+		fmt.Println("Error: expected exactly one argument")
 		return
 	}
 
@@ -54,8 +61,8 @@ func exit(args []string) {
 
 func echo(args []string) {
 	if len(args) < 1 {
-		fmt.Println("Error: not enough arguments")
-		return 
+		fmt.Println("Error: not enough aguments")
+		return
 	}
 	fmt.Println(strings.Join(args, " "))
 }
@@ -65,10 +72,21 @@ func type_(args []string) {
 		fmt.Println("Error: expected exactly one argument")
 		return
 	}
+	command := args[0]
 
-	if _, exists := COMMANDS[args[0]]; exists {
+	if _, exists := COMMANDS[command]; exists {
 		fmt.Println(args[0] + " is a shell builtin")
 	} else {
+		pathEnv := os.Getenv("PATH")
+		paths := strings.Split(pathEnv, ":")
+
+		for _, dir := range paths {
+			fullPath := filepath.Join(dir, command)
+			if info, err := os.Stat(fullPath); err == nil && info.Mode().IsRegular() && info.Mode().Perm()&0111 != 0 {
+				fmt.Println(command + " is " + fullPath)
+				return
+			}
+		}
 		fmt.Println(args[0] + ": not found")
 	}
 }
